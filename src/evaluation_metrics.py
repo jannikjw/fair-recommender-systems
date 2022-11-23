bcc290d0eb8899860544fd25416e7973a2eb32ce
Madison Grace Thantu <madisonthantu@gmail.com>
2022-11-23 00:21:47 -0500

    Metrics include slate_diversity, novelty, serendipity, spread + helper functions

--- src/evaluation_metrics.py
+++ src/evaluation_metrics.py
@@ -2,7 +2,7 @@ import math
 
 """
 Assumptions:
--   Item object has an attribute n_users, which is a count of the number of users that have 
+-   Item object has an attribute num_users, which is a count of the number of users that have 
     consumed this piece of content
 """
 
@@ -23,17 +23,23 @@ def slate_diversity(slate):
 
 """
 Item-level self-information - measures the unexpectedness of a recommended item relative to its global popularity
-*Assume Item object has an attribute n_users, which is a count of the number of users that have consumed this piece of content
+* Assume *  Item object has an attribute num_users, which is a count of the number of users that have consumed this piece of content
+            Note that num_users is an attribute defined in the ActualUserProfiles class
 """
-def self_information(item, n_users):
-    return (-1) * math.log((item.n_users*1.0) / n_users)    
+def self_information(item, num_users):
+    return (-1) * math.log((item.num_users*1.0) / num_users)    
 
 """
 Metric to capture the global popularity-based measurements. Computing the novelty of a slate that is presented to a user.
 """
-def novelty(slate, n_users):
-    return sum([self_information(item, n_users) for item in slate])
+def novelty(slate, num_users):
+    return sum([self_information(item, num_users) for item in slate])
 
+"""
+Item-level serendipity
+TO DO:  Define some metric that evaluates item-wise serendipity. This could incorporate our previous implementations for 
+        self_information and similarity/dissimilarity (the latter of which could be w.r.t. user.state_history)
+"""
 def item_serendipity(item, user):
     return
 
@@ -43,5 +49,17 @@ Metric to capture the unexpectedness/surprise of the recommendation to a specifi
 def serendipity(slate, user):
     return sum([item_serendipity(item, user) for item in slate])
 
-def spread():
-    return
+
+"""
+Function to compute the distance in leaning between two items
+"""
+def leaning_distance(item1, item2):
+    dist = math.abs(item1.leaning - item2.leaning)
+    return dist**2
+
+"""
+Metric to capture the spread of *leanings* in a given slate of items
+"""
+def spread(slate):
+    sorted = slate.sorted()
+    return sum([leaning_distance(slate[i-1], slate[i]) for i in slate[1:]])
