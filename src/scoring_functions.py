6a6c107c853f17c098a53ff092af173fc84dec8b
jannikjw <jayjay1110@hotmail.de>
2022-12-11 20:39:01 +0000

    Documented content_fairness

--- src/scoring_functions.py
+++ src/scoring_functions.py
@@ -39,31 +39,62 @@ def entropy(predicted_user_profiles, predicted_item_attributes):
 
 
 def content_fairness(predicted_user_profiles, predicted_item_attributes):
+    """
+    A score function that ensures that no topic is overrepresented. 
+    1. Scores are predicted using the inner product (myopic).
+    2. Probabilities are created based on these scores to ensure common value range
+    3. The weight of an item to a topic is determined based on its relative value in its embeddings
+    4. A slate is created based on myopic scores.
+    5. If adding a new item to the slate would exceed the upper bound in a topic dimension, the item is not added.
+       Instead, the function proceeds to the next available item until one is found that does not exceed any weights
+       in the topic dimensions.
+    6. To ensure that the items appear in the top-k slate picked by t-recs, the scores of the slate items are
+       manually increased to be in the top-k.
+    
+    Caveats: Upper_bound and slate_size are set manually because default t-recs implementation does not allow
+    passing additional parameters to a score function. The current implementation only works with a top-k slate (non-probabilistic). Also scores are manually altered which reduces interpretability of the scores.
+    
+    Based on "Controlling Polarization in Personalization: An Algorithmic Framework" by Celis et. al.
+    
+    Inputs:
+        - predicted_user_profiles
+        - pretedicted_item_attributes
+    Outputs:
+        - predicted_scores_reranked (Shape: U x I)
+    """
     slate_size = 10
     upper_bound = 0.75
 
-
+    # 1. Calculate myopic scores
     predicted_scores =  mo.inner_product(predicted_user_profiles, predicted_item_attributes)
-    probs = (predicted_scores.T / np.sum(predicted_scores, axis=1)).T
 
+    # 2. Calculate probabilities and sort them
+    probs = (predicted_scores.T / np.sum(predicted_scores, axis=1)).T
     probs_sorted = np.flip(np.argsort(probs, axis=1), axis=1)
     
+    # 3. Determine weight per embedding dimension for each item (scaled to [0,1])
     gw = predicted_item_attributes.T / np.sum(predicted_item_attributes.T, axis=1)[:, np.newaxis]
 
     num_user = len(predicted_user_profiles)
     recs = np.empty((num_user, slate_size))
+    
     for user in range(len(probs_sorted)):
-        agg_weight_per_cluster = np.zeros((len(gw[0])))
+        agg_weight_per_cluster = np.zeros((len(gw[0]))) # matrix to keep track of weights of slate 
+        
         i = 0
+        # 4. Create slate in order of myopic scores
         for item in probs_sorted[user]:
             weight_item = gw[item]
             # print(f'{item}: {weight_item}')
+            
+            # 5. Calculate weights if item was added to slate. If exceeds upper_bound go to next item.
             proposed_weights = weight_item + agg_weight_per_cluster
             if (proposed_weights <= upper_bound).all() and i < slate_size:
                 agg_weight_per_cluster = proposed_weights
                 recs[user, i] = item
                 i += 1
 
+    # 6. Manually assign new scores to items in slate
     predicted_scores_reranked = np.copy(predicted_scores)
     for i, user in enumerate(recs):
         for item in np.flip(user):
